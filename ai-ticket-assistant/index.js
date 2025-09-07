059b199aa8eb736084ab666fc2ad56002bbd9dd2
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-09-07 08:38:42 +0530

    index file

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import mongoose from "mongoose"
 import cors from "cors"
+import { serve } from "inngest/express"
+
 import userRoutes from "./routes/user.js"
+import ticketRoutes from "./routes/ticket.js"
+import { inngest } from "./inngest/client.js";
+import {onUserSignup} from "./inngest/functions/on-signup.js"
+import {onTicketCreated} from "./inngest/functions/on-ticket.js"
+
+import dotenv from "dotenv"
+dotenv.config()
 
 const PORT = process.env.PORT || 3000
 const app = express()
@@ -10,6 +19,12 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/auth", userRoutes)
+app.use("/api/tickets", ticketRoutes)
+
+app.use("/api/inngest", serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated]
+}))
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
