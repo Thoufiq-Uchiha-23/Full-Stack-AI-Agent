bdab9fc563a9b34648aba5564b3a140c4c143545
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-09-08 09:12:07 +0530

    CheckAuth page

--- ai-ticket-frontend/src/components/CheckAuth.jsx
+++ ai-ticket-frontend/src/components/CheckAuth.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+const CheckAuth = ({ children, protectedRoute }) => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-const CheckAuth = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  useEffect(() => {
+    const token = localStorage.getItem("token");
 
-export default CheckAuth
+    if (protectedRoute) {
+      if (!token) {
+        navigate("/login");
+      } else {
+        setLoading(true);
+      }
+    } else {
+      if (token) {
+        navigate("/");
+      } else {
+        setLoading(false);
+      }
+    }
+  }, [navigate, protectedRoute]);
+
+  if(loading) {
+    return <div>loading...</div>
+  }
+
+  return children;
+};
+
+export default CheckAuth;
