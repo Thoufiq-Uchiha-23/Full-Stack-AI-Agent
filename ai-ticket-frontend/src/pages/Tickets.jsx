4c17be2671ede2577607dd4e8a1e1303e72e5fda
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-09-10 09:23:09 +0530

    Ticket fix

--- ai-ticket-frontend/src/pages/Tickets.jsx
+++ ai-ticket-frontend/src/pages/Tickets.jsx
@@ -10,7 +10,7 @@ export default function Tickets() {
 
   const fetchTickets = async () => {
     try {
-      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/tickets`, {
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/tickets`, {
         headers: { Authorization: `Bearer ${token}` },
         method: "GET",
       });
