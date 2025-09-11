b4accbd0712dfc1de02517b3ae4d5d5854abe8e5
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-09-11 20:05:21 +0530

    models modification

--- ai-ticket-assistant/models/user.js
+++ ai-ticket-assistant/models/user.js
@@ -8,4 +8,22 @@ const userSchema = new mongoose.Schema({
     createdAt: {type: Date, default: Date.now}
 })
 
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// // check and match password method
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
+//   return isPasswordCorrect;
+// };
+
 export default mongoose.model("User", userSchema)
